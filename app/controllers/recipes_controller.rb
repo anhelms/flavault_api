cfab3bf2f61e8d5355095f54619551b0854f0bc9
Anna N Helms <unicornbuddha@gmail.com>
2025-01-28 16:40:15 -0800

    Add create action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,4 +3,18 @@ class RecipesController < ApplicationController
         @recipes = Recipe.all 
         render :index
     end
+
+    def create
+        @recipe = Recipe.create(
+            title: params[:title],
+            prep_time: params[:prep_time],
+            servings: params[:servings],
+            description: params[:description],
+            ingredients: params[:ingredients],
+            directions: params[:ingredients],
+            chef: params[:chef],
+            image_url: params[:image_url],
+        )
+        render :show
+    end
 end
--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,11 +1,18 @@
 require "test_helper"
 
 class RecipesControllerTest < ActionDispatch::IntegrationTest
-    test "index" do
+  test "index" do
     get "/recipes.json"
     assert_response 200
 
     data = JSON.parse(response.body)
     assert_equal Recipe.count, data.length
+  end
+
+  test "create" do
+    assert_difference "Recipe.count", 1 do
+      post "/recipes.json", params: { title: "Cooked Eggs", prep_time: 5, servings: 1, description: "2 sunny side up eggs", ingredients: "2 eggs", directions: "Crack open two egg shells into a pan and fry until desired level of cooked", chef: "Anna Helms", image_url: "https://www.southernliving.com/thmb/NDuJP6OZAhYDO-3xexTbcdxZGds=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Sunny_Side_Up_Eggs_008-c7f1db520c2a476db6b9e84e60f8c119.jpg" }
+      assert_response 200
+    end
   end
 end
